ff5dcc1b2d83406d6775b53a75a6cba73966572a
siska <dolyants@gdtltd.com>
2021-03-01 18:07:34 +0300

    close project day1_1

--- day1_1/main.c
+++ day1_1/main.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	signed long int n1;
+	long n2;
+	
+	double a,b=7,c=2;
+	a=b/c;
+	
+	int k1=5;
+	long long k2=3uLL;
+	double k3=.4;
+	float k4=4.1f;
+	
+	printf("%i %lld",k1,12345678913);
+	
 	return 0;
 }
